18eea3601001c1bc0bbd5a7aaf42a5e5dc44e18f
unknown <daniel.wang@pu-ws-130.queen.pageup.com.au>
2014-08-07 17:26:12 +1000

    add search

---
+++ js/learning.js
@@ -0,0 +1,55 @@
+
+		//MASONRY
+		$(document).ready(function(){
+			var $container = $('#Lcontent');
+			// init
+			$container.isotope({
+			  // options
+			  columnWidth: 270,
+			  itemSelector: '.item',
+			  layoutMode: 'masonry'
+			  
+			});
+
+			//FILTER
+			$('#filter a').click(function(){
+			  var selector = $(this).attr('data-filter');
+				$container.isotope({ 
+				filter: selector,
+				animationOptions: {
+				 duration: 750,
+				 easing: 'linear',
+				 queue: false,
+			   }
+			  });
+			  return false;
+			});
+			$('#filter a').click(function (event) {
+				$('a.selected').removeClass('selected');
+				var $this = $(this);
+				$this.addClass('selected');
+				var selector = $this.attr('data-filter');
+				$container.isotope({
+					 filter: selector
+				});
+				return false;
+				});
+
+			$('#searchbox').keydown(function(){
+				$('#autosuggestion').show();
+				var $this = $(this);
+				var lng = $this.val().length;
+				if( lng >10){
+					$('.media').fadeOut();
+					$('#add-msg').fadeIn();
+				}else{
+					$('.media').fadeIn();
+					$('#add-msg').fadeOut();
+				}
+			});
+
+			$('#searchbox').focusout(function(){
+				$('#autosuggestion').hide();
+			});
+
+		});
