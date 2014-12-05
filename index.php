6001f5e74796745d6f6549f26c4351f17f31a247
danielwang <danielwang@Daniels-Air.BigPond>
2014-12-06 00:34:31 +1100

    upgrade 2 to 3

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php include '_partials/head.html';?>
 <?php include '_partials/nav.html';?>
+
+
     <div class="container">
       <div id="dockbody">
         <div class="row row-gap">
-          <div id="middleColumn" class="span6 offset3">
+          <div id="middleColumn" class="col-lg-6 col-lg-offset-3">
                <?php include '_partials/journal.php';?>
                <?php include '_partials/timeline.php';?>
           </div>
