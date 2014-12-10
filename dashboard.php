49a8e49b153c1317c10163032a9aa5260573a973
danielwang <danielwang@Daniels-Air.BigPond>
2014-12-10 22:57:46 +1100

    circles on dashboard

---
+++ dashboard.php
@@ -0,0 +1,330 @@
+<?php include '_partials/head.html';?>
+<?php include '_partials/nav.html';?>
+    
+<div class="container">
+	<div class="row clearfix">
+		<div class="col-md-12 column">
+			<div class="row clearfix">
+				<div class="col-md-8 column">     
+			        <div class="box">
+			        <h4><i class="fa fa-suitcase"></i> Current Jobs</h4>
+			              <ul class="status clearfix">  
+			                <li data-attr="all">
+			                    <div class="c100 c0 dark small p100">
+			                        <span>10</span>
+			                        <div class="slice">
+			                            <div class="bar"></div>
+			                            <div class="fill"></div>
+			                        </div>
+			                    </div> 
+			                    <span>Total</span> 
+			                </li>
+			                <li data-attr="s1">
+			                    <div class="c100 c1 blue small p100">
+			                        <span>2</span>
+			                        <div class="slice">
+			                            <div class="bar"></div>
+			                            <div class="fill"></div>
+			                        </div>
+			                    </div> 
+			                    <span>Sourcing</span> 
+			                    <i class="alert fa fa-exclamation-circle fa-lg"></i>
+			                </li>
+			                <li data-attr="s2">
+			               
+			                    <div class="c100 c2 orange small p100">
+			                        <span>1</span>
+			                        <div class="slice">
+			                            <div class="bar"></div>
+			                            <div class="fill"></div>
+			                        </div>
+			                    </div>
+			                    <span>Screening</span>  
+			                </li>
+			                <li data-attr="s3">
+			                
+			                    <div class="c100 c3 purple small p100">
+			                        <span>1</span>
+			                        <div class="slice">
+			                            <div class="bar"></div>
+			                            <div class="fill"></div>
+			                        </div>
+			                    </div> 
+			                    <span>Interview</span>
+			                    <i class="alert fa fa-exclamation-circle fa-lg"></i>  
+			                </li>
+			                <li data-attr="s4">
+			               
+			                    <div class="c100 c4 yellow small p100">
+			                        <span>1</span>
+			                        <div class="slice">
+			                            <div class="bar"></div>
+			                            <div class="fill"></div>
+			                        </div>
+			                    </div> 
+			                    <span>Background Checking </span> 
+			                
+			                </li>
+			                <li data-attr="s5">
+			                          
+			                    <div class="c100 c5 small green p100">
+			                        <span>4</span>
+			                        <div class="slice">
+			                            <div class="bar"></div>
+			                            <div class="fill"></div>
+			                        </div>
+			                    </div> 
+			                     <span>Offer</span>    
+			                </li>
+			            </ul>
+
+			            <div class="job s1">
+			                <div class="app col-sm-2">
+			                    <a href="#"><i class="fa fa-user"></i> 27</a>
+			                    <P>
+			                        <label class="new">2 new</label>
+			                    </p>
+			                </div>
+			                <div class="col-sm-10">
+			                    <h4><a href="#"><i class="fa fa-lg fa-info-circle"></i> Global Transaction Banking Sales</a></h4>
+			                    <span class="col-sm-4">Job No.: 691858</span>
+			                    <span class="col-sm-4">John Smith</span>
+			                    <span class="col-sm-4">Positon: 1</span>
+			                       <ul class="col-sm-12">
+			                        <li><i class="fa fa-bell alert swing animated infinite"></i> 1 application in Background check for more than 3 days.<a href="#">View</a></li>
+			                        <li><i class="fa fa-circle-o"></i> 1 assigned application is approved. <a href="#">View</a></li>
+			                        <li><i class="fa fa-bell alert swing animated infinite"></i> 5 applications in Approved to continue selection for more than 1 hour <a href="#">View</a></li>
+			                       </ul>
+			                </div>
+			                <br clear="all">
+			            </div>
+			            <div class="job s1">
+			                    <div class="app col-sm-2">
+			                    <a href="#"><i class="fa fa-user"></i> 15</a>
+			                    <P>
+			                        <label class="new">5 new</label>
+			                    </p>
+			                </div>
+			                <div class="col-sm-10">
+			                    <h4><a href="#"><i class="fa fa-lg fa-info-circle"></i>  Senior Consultant </a></h4>
+			                    <span class="col-sm-4">Job No.: 691858</span>
+			                    <span class="col-sm-4">John Smith</span>
+			                    <span class="col-sm-4">Positon: 1</span>
+			                    <ul class="clearfix">                   
+			                        <li><i class="fa fa-circle-o"></i> 1 assigned application is approved. <a href="#">View</a></li>                    
+			                    </ul>
+			                </div>
+			                    <br clear="all">
+			            </div>
+			            <div class="job s2">
+			                    <div class="app col-sm-2">
+			                    <a href="#"><i class="fa fa-user"></i> 12</a>
+			                </div>
+			                    <div class="col-sm-10">
+			                    <h4><a href="#"><i class="fa fa-lg fa-info-circle"></i>  Business Banking Manager </a></h4>
+			                    <span class="col-sm-4">Job No.: 691858</span>
+			                    <span class="col-sm-4">John Smith</span>
+			                    <span class="col-sm-4">Positon: 1</span>
+			                </div>
+			                    <br clear="all">
+			            </div>
+			            <div class="job s2">
+			                <div class="app col-sm-2">
+			                    <a href="#"><i class="fa fa-user"></i> 31</a>
+			                </div>
+			                <div class="col-sm-10">
+			                    <h4><a href="#"><i class="fa fa-lg fa-info-circle"></i> Sales Manager</a></h4>
+			                    <span class="col-sm-4">Job No.: 691858</span>
+			                    <span class="col-sm-4">John Smith</span>
+			                    <span class="col-sm-4">Positon: 1</span>
+			                </div>
+			                    <br clear="all">
+			            </div>
+			            <div class="job s3">
+			                <div class="app col-sm-2">
+			                    <a href="#"><i class="fa fa-user"></i> 36</a>
+			                    <P>
+			                        <label class="new">12 new</label>
+			                    </p>
+			                </div>
+			                <div class="col-sm-10">
+			                    <h4><a href="#"><i class="fa fa-lg fa-info-circle"></i> Regional Sales Manager</a></h4>
+			                    <span class="col-sm-4">Job No.: 691858</span>
+			                    <span class="col-sm-4">John Smith</span>
+			                    <span class="col-sm-4">Positon: 1</span>
+			                    <ul class="clearfix">
+			                    <li><i class="fa fa-bell alert swing animated infinite"></i> 1 application in Background check for more than 3 days.<a href="#">View</a></li>
+			                    <li><i class="fa fa-circle-o"></i> 1 assigned application is approved. <a href="#">View</a></li>
+			                    <li><i class="fa fa-bell alert swing animated infinite"></i> 5 applications in Approved to continue selection for more than 1 hour <a href="#">View</a></li>
+			                    </ul>
+			                </div>
+			                    <br clear="all">
+			            </div>
+
+			                <div class="job s4">
+			                    <div class="app col-sm-2">
+			                    <a href="#"><i class="fa fa-user"></i> 11</a>
+			                    <P>
+			                        <label class="new">7 new</label>
+			                    </p>
+			                </div>
+			                <div class="col-sm-10">
+			                    <h4><a href="#"><i class="fa fa-lg fa-info-circle"></i> Store Manager</a></h4>
+			                    <span class="col-sm-4">Job No.: 691858</span>
+			                    <span class="col-sm-4">John Smith</span>
+			                    <span class="col-sm-4">Positon: 1</span>
+			                </div>
+			                <br clear="all">
+			            </div>
+			            <div class="job s5">
+			                    <div class="app col-sm-2">
+			                    <a href="#"><i class="fa fa-user"></i> 37</a>
+			                    <P>
+			                        <label class="new">22 new</label>
+			                    </p>
+			                </div>
+			                <div class="col-sm-10">
+			                    <h4><a href="#"><i class="fa fa-lg fa-info-circle"></i> Account Manager </a></h4>
+			                    <span class="col-sm-4">Job No.: 691858</span>
+			                    <span class="col-sm-4">John Smith</span>
+			                    <span class="col-sm-4">Positon: 1</span>
+			                </div>
+			                    <br clear="all">
+			            </div>
+			            <div class="job s5">
+			                <div class="app col-sm-2">
+			                    <a href="#"><i class="fa fa-user"></i> 29</a>
+			                    <P>
+			                        <label class="new">25 new</label>
+			                    </p>
+			                </div>
+			                <div class="col-sm-10">
+			                    <h4><a href="#"><i class="fa fa-lg fa-info-circle"></i> Senior Administration Assistant</a></h4>
+			                    <span class="col-sm-4">Job No.: 691858</span>
+			                    <span class="col-sm-4">John Smith</span>
+			                    <span class="col-sm-4">Positon: 1</span>
+			                </div>
+			                    <br clear="all">
+			            </div>
+			            <div class="job s5">
+			                    <div class="app col-sm-2">
+			                    <a href="#"><i class="fa fa-user"></i> 61</a>
+			                    <P>
+			                        <label class="new">18 new</label>
+			                    </p>
+			                </div>
+			                <div class="col-sm-10">
+			                    <h4><a href="#"><i class="fa fa-lg fa-info-circle"></i> Customer Service Consultant</a></h4>
+			                    <span class="col-sm-4">Job No.: 691858</span>
+			                    <span class="col-sm-4">John Smith</span>
+			                    <span class="col-sm-4">Positon: 1</span>
+			                </div>
+			                    <br clear="all">
+			            </div>
+			            <div class="job s5">
+			                <div class="app col-sm-2">
+			                    <a href="#"><i class="fa fa-user"></i> 45</a>
+			                </div>
+			                    <div class="col-sm-10">
+			                    <h4><a href="#"><i class="fa fa-lg fa-info-circle"></i>  General Manager - Engineering </a></h4>
+			                    <span class="col-sm-4">Job No.: 691858</span>
+			                    <span class="col-sm-4">John Smith</span>
+			                    <span class="col-sm-4">Positon: 1</span>
+			                </div>
+			                    <br clear="all">
+			            </div>
+			        </div>
+
+
+				</div>
+				<div class="col-md-4 column">
+
+					  <div class="event box">
+			            <h4> <i class="fa fa-calendar"></i> Upcoming events</h4>
+			            <ul>
+			                <li class="date">Tomorrow </li>
+			                <li>
+			                    <a href="#"><i class="fa fa-lg fa-info-circle"></i> 3:00 pm to 4:00 pm Phone Interview with Steve Tout.</a>
+			                </li>
+			                <li>
+			                    <a href="#"><i class="fa fa-lg fa-info-circle"></i> 3:00 pm to 4:00 pm Phone Interview with Steve Tout.</a>
+			                </li>
+			                  <li>
+			                    <a href="#"><i class="fa fa-lg fa-info-circle"></i> 3:00 pm to 4:00 pm Phone Interview with Steve Tout.</a>
+			                </li>
+			               
+			                <li class="date"> 15 Nov </li>
+			                <li>
+			                    <a href="#"><i class="fa fa-lg fa-info-circle"></i> 1:00 pm to 4:00 pm Phone Interview with Steve Tout.</a>
+			                </li>
+			            </ul>
+			        </div>
+
+			     
+			        <div class="tasks box">
+			            <h4><i class="fa fa-tasks"></i> Tasks</h4>
+			            <p><a href="#">Assigned Tasks</a> <button class="button right"><i class="fa fa-plus-circle"></i> Add</button> </p>
+			            <hr />
+			            <div class="task">
+			                <div class="col-sm-2 date">
+			                    <i class="fa fa-level-up alert"></i>
+			                    <span>12 Dec 2014</span>
+			                </div>
+			                <div class="col-sm-10 content">
+			                    <p><strong> Follow up with Bob Jones </strong></p>
+			                    
+			                </div>
+			            </div>
+			            <div class="task">
+			                <div class="col-sm-2 date">
+			                    <i class="fa fa-clock-o alert"></i>                   
+			                    <span class="alert">9 Nov 2014</span>
+			                </div>
+			                <div class="col-sm-10 content">
+			                    <p>Call: Jane Disby - Congratulations</p>
+			                </div>
+			            </div>
+			             <div class="task">
+			                <div class="col-sm-2 date">
+			                    <i class="fa fa-level-down"></i>                   
+			                    <span>9 Jan 2015</span>
+			                </div>
+			                <div class="col-sm-10 content">
+			                    <p><a href="#"><i class="fa fa-info-circle"></i></a> Head of Delivery (Brisbane) 10020371-003 </p>
+			                </div>
+			            </div>
+
+			            <br clear="both" />
+			        </div>   <!-- end tasks--> 
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- ******************************************************************************************************  -->      
+<script>
+    Event.observe(window, "load", function () {
+
+       $$('.status li').each(function (el) {
+          el.observe('click', function(event) {
+             var status = el.readAttribute('data-attr');
+             $$('.job').each(function (el2) {
+                if (status == 'all'){
+                   el2.removeClassName('hidden');
+                } 
+                else if (!el2.hasClassName(status)){
+                       el2.addClassName('hidden');
+                    } 
+                else{
+                    el2.removeClassName('hidden');
+
+                }
+             });
+          });
+
+       });
+    });
+
+</script>   
+<?php include '_partials/foot.html';?>  
