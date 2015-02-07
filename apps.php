7ea6cf05ff8f4e203c945408244db5abb86f98b0
danielwang <danielwang@Daniels-Air.BigPond>
2015-02-08 09:57:10 +1100

    add apps page

---
+++ apps.php
@@ -0,0 +1,68 @@
+<?php include '_partials/head.html';?>
+<?php include '_partials/nav.html';?>
+    <div class="container">
+    <div class="row clearfix">
+      <div class="col-xs-12 col-md-4 col-md-offset-4">
+        <div class="row clearfix">
+          <div class="col-xs-4">
+             <div class="pu-square">
+                
+            </div> 
+            <span>Recuitment</span> 
+          </div>
+          <div class="col-xs-4 ">
+            <div class="pu-square">
+                <i class="fa fa-star"></i>            
+            </div> 
+            <span>Performance</span> 
+          </div>
+          <div class="col-xs-4">
+            <div class="pu-square">
+                <span><i class="fa fa-graduation-cap"></i></span>   
+            </div> 
+            <span>Learning</span> 
+          </div>
+           <div class="col-xs-4">
+             <div class="pu-square">
+               
+            </div> 
+            <span>360</span> 
+          </div>
+          <div class="col-xs-4 ">
+            <div class="pu-square">
+                <span><i class="fa fa-star"></i></span>
+            </div> 
+            <span>Timesheet</span> 
+          </div>
+          <div class="col-xs-4">
+            <div class="pu-square">
+                <span><i class="fa fa-graduation-cap"></i></span>
+            </div> 
+            <span>Compensation</span> 
+          </div>
+           <div class="col-xs-4">
+             <div class="pu-square">
+                <span>10</span>
+                
+            </div> 
+            <span>Onboarding</span> 
+          </div>
+          <div class="col-xs-4 ">
+            <div class="pu-square">
+                <span><i class="fa fa-star"></i></span>
+                
+            </div> 
+            <span>Journal</span> 
+          </div>
+          <div class="col-xs-4">
+            <div class="pu-square">
+                <span><i class="fa fa-star"></i></span>
+                
+            </div> 
+            <span>Succession</span> 
+          </div>
+      </div>
+      </div>
+    </div>
+  </div>
+<?php include '_partials/foot.html';?>  
