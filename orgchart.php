3ca7a4aadc7d558acad2c586c01214fc5655dd86
danielwang <danielwang@daniels-air.queen.pageup.com.au>
2015-02-10 16:09:34 +1100

    iframed the org chart
    iframed the org chart

---
+++ orgchart.php
@@ -0,0 +1,6 @@
+<?php include '_partials/head.html';?>
+<?php include '_partials/nav.html';?>
+
+<iframe src="orgchart/index.php" onload="this.width=window.innerWidth;this.height=window.innerHeight;" frameborder="0"></iframe>
+
+<?php include '_partials/foot.html';?>
