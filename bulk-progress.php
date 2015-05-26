3e7127debc9302b02d2ac0d44fcef339a99d1018
danielwang <danielwang@Daniels-Air.gateway>
2015-05-26 20:55:05 +1000

    add circle progress ui

---
+++ bulk-progress.php
@@ -0,0 +1,77 @@
+<?php include '_partials/head.html';?>
+<?php include '_partials/nav.html';?>
+
+<div class="container">
+	<br><br>
+	
+   	<div class="row">
+      <div class="col-sm-8 col-sm-offset-2">
+      	<h4>Bulk creating 1000 Performance reviews</h4>
+      	<hr>
+      	<div class="row">
+	        <span class="col-xs-6">
+	            <div class="c100 orange center big p65 selected">
+	                <span>650<sub>Precessed</sub></span>
+
+	                <div class="slice">
+	                    <div class="bar"></div>
+	                    <div class="fill"></div>
+	                </div>
+	            </div>
+	        </span>
+	        <span class="col-xs-6">
+
+	        	<span class="col-xs-6 text-center">
+		            <div class="c100 center small green p97">
+		                <span>630</span>
+
+		                <div class="slice">
+		                    <div class="bar"></div>
+		                    <div class="fill"></div>
+		                </div>
+		            </div>
+		            <h5 class="green">Success</h5>
+		        </span>    
+		         <span class="col-xs-6 text-center">    
+		           	<div class="c100 center small red p3">
+		                <span>20</span>
+
+		                <div class="slice">
+		                    <div class="bar"></div>
+		                    <div class="fill"></div>
+		                </div>
+		            </div>
+	            	<h5 class="red">Warning</h5>
+	            </span>
+	        </span>
+	    </div> 
+	    <br>  <br>
+	    <div class="row">
+		    <div class="alert alert-danger">	
+			  <p>
+		        <a href="#" class="alert-link">Peter Smith</a> has no manager
+		      </p>
+		      <p>
+		        <a href="#" class="alert-link">Daniel Wang</a> has no review process
+		      </p>
+		      <p>
+		        <a href="#" class="alert-link">Adam Newton</a> has no review process
+		      </p>
+		      <p>
+		        <a href="#" class="alert-link">Marcus Wong</a> has no manager 
+		      </p>
+		      <p>
+		        <a href="#" class="alert-link">Peter Smith</a>has no review process
+		      </p>
+		    </div>
+	    </div>   
+      </div>
+<!---  *************************************--> 
+
+   </div> 
+
+   
+    
+</div>
+
+<?php include '_partials/foot.html';?>
