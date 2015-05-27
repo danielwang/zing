32c8fe5c4fbda0e004f129934b0a9207a703793f
danielwang <danielwang@Daniels-Air.gateway>
2015-05-27 22:27:41 +1000

    add component menu

--- bulk-progress.php
+++ bulk-progress.php
@@ -6,12 +6,12 @@
 	
    	<div class="row">
       <div class="col-sm-8 col-sm-offset-2">
-      	<h4>Bulk creating 1000 Performance reviews</h4>
+      	<h4>Bulk creating <strong style="color:#e8910c">1000</strong> Performance reviews</h4>
       	<hr>
-      	<div class="row">
+      	<div class="row boxcontainer">
 	        <span class="col-xs-6">
 	            <div class="c100 orange center big p65 selected">
-	                <span>650<sub>Precessed</sub></span>
+	                <span>650<sub>Processed</sub></span>
 
 	                <div class="slice">
 	                    <div class="bar"></div>
@@ -20,50 +20,59 @@
 	            </div>
 	        </span>
 	        <span class="col-xs-6">
+	        	<div class="row clearfix">
+		        	<span class="col-xs-6 text-center">
+			            <div class="c100 center small green p97">
+			                <span>630</span>
 
-	        	<span class="col-xs-6 text-center">
-		            <div class="c100 center small green p97">
-		                <span>630</span>
+			                <div class="slice">
+			                    <div class="bar"></div>
+			                    <div class="fill"></div>
+			                </div>
+			            </div>
+			            <h5 class="green">Success</h5>
+			        </span>    
+			         <span class="col-xs-6 text-center">    
+			           	<div class="c100 center small red p3">
+			                <span>20</span>
 
-		                <div class="slice">
-		                    <div class="bar"></div>
-		                    <div class="fill"></div>
-		                </div>
-		            </div>
-		            <h5 class="green">Success</h5>
-		        </span>    
-		         <span class="col-xs-6 text-center">    
-		           	<div class="c100 center small red p3">
-		                <span>20</span>
-
-		                <div class="slice">
-		                    <div class="bar"></div>
-		                    <div class="fill"></div>
-		                </div>
-		            </div>
-	            	<h5 class="red">Warning</h5>
-	            </span>
+			                <div class="slice">
+			                    <div class="bar"></div>
+			                    <div class="fill"></div>
+			                </div>
+			            </div>
+		            	<h5 class="red">Warning</h5>
+		            </span>
+	            </div>
+	            <hr>
+	            <div class="row clearfix">
+		            <div class="col-xs-12 alert alert-danger">	
+					  <p>
+				        <a href="#" class="alert-link">Peter Smith</a> has no manager
+				      </p>
+				      <p>
+				        <a href="#" class="alert-link">Daniel Wang</a> has no review process
+				      </p>
+				      <p>
+				        <a href="#" class="alert-link">Adam Newton</a> has no review process
+				      </p>
+				      <p>
+				        <a href="#" class="alert-link">Marcus Wong</a> has no manager 
+				      </p>
+				      <p>
+				        <a href="#" class="alert-link">Peter Smith</a> has no review process
+				      </p>
+				      <p><a href="#">more</a> (15) </p>
+				    </div>
+				</div>
 	        </span>
+
 	    </div> 
-	    <br>  <br>
-	    <div class="row">
-		    <div class="alert alert-danger">	
-			  <p>
-		        <a href="#" class="alert-link">Peter Smith</a> has no manager
-		      </p>
-		      <p>
-		        <a href="#" class="alert-link">Daniel Wang</a> has no review process
-		      </p>
-		      <p>
-		        <a href="#" class="alert-link">Adam Newton</a> has no review process
-		      </p>
-		      <p>
-		        <a href="#" class="alert-link">Marcus Wong</a> has no manager 
-		      </p>
-		      <p>
-		        <a href="#" class="alert-link">Peter Smith</a>has no review process
-		      </p>
-		    </div>
+	    <br>
+	    <div class="row text-center">
+	    	<p>
+	    		<input id="agree" type="checkbox" /> <label for="agree">Running as background, notify me when finishes</label>
+                <button class="btn" style="background:#0D6083; color:#fff">OK</button></p>           
 	    </div>   
       </div>
 <!---  *************************************--> 
