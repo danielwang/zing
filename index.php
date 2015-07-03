dfcd5c5a4d51c424ef6879a64302a736479e8b89
danielwang <danielwang@Daniels-Air.gateway>
2015-07-03 22:46:24 +1000

    dock

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php include 'base.php' ?>
+
+<?php startblock('page') ?>
+    <div class="row clearfix">
+      <div class="col-xs-12 col-md-4 col-md-offset-4">
+        <div class="row clearfix">
+          <div class="col-xs-4 text-center">
+            <a href="learning.php"><span class="pu-square orange-bg">
+                <span><i class="fa fa-graduation-cap"></i></span>   
+            </span></a> 
+            <span>Learning</span> 
+          </div>
+          <div class="col-xs-4 text-center">
+            <a href="journal.php">
+              <span class="pu-square purple-bg">
+                <span><i class="fa fa-pencil-square-o"></i></span>
+              </span>   
+            </a> 
+            <span>Journal</span> 
+          </div>
+
+          <div class="col-xs-4 text-center">
+             <a href="dashboard.php">
+              <span class="pu-square blue-bg">
+                 <i class="fa fa-user-plus"></i>   
+              </span>
+            </a> 
+            <span>Recuitment</span> 
+          </div>
+          <div class="col-xs-4 text-center">
+            <a href="performance.php">
+                <span class="pu-square green-bg">
+                  <i class="fa fa-star"></i>
+                </span>            
+            </a> 
+            <span>Performance</span> 
+          </div>
+          <div class="col-xs-4 text-center">
+             <div class="pu-square red-bg">
+                <span><i class="fa fa-usd"></i></span>            
+            </div> 
+            <span>Expense</span> 
+          </div>
+          
+           <div class="col-xs-4 text-center">
+             <div class="pu-square sushi-bg">
+               <span><i class="fa fa-calendar"></i></span>   
+            </div> 
+            <span>Leave</span> 
+          </div>
+          <div class="col-xs-4 text-center">
+            <div class="pu-square blue-bg">
+                <span><i class="fa fa-clock-o"></i></span>
+            </div> 
+            <span>Timesheet</span> 
+          </div>
+          <div class="col-xs-4 text-center">
+            <div class="pu-square green-bg">
+                <span><i class="fa fa-money"></i></span>
+            </div> 
+            <span>Compensation</span> 
+          </div>
+          
+          <div class="col-xs-4 text-center">
+            <div class="pu-square red-bg">
+              <span><i class="fa fa-suitcase"></i></span>           
+            </div>
+            <span>Travel</span> 
+          </div>
+      </div>
+      </div>
+    </div>
+<?php endblock() ?>
