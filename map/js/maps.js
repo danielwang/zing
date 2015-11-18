31c4f36a29b01d0372afe7d26ba8ee53d7853501
danielwang <danielwang@Daniels-Air.gateway>
2015-11-18 17:38:17 +1100

    load initial markers

--- map/js/maps.js
+++ map/js/maps.js
@@ -81,7 +81,7 @@ window.onload = initMap;
 
 // add markers on map
 function AddMakers(){
-  /* var json = (function() {
+   var json = (function() {
         var json = null;
         $.ajax({
             'async': false,
@@ -100,8 +100,8 @@ function AddMakers(){
    for (var i = 0, length = json.length; i < length; i++) {
         var data = json[i],
         latLng = new google.maps.LatLng(data.lat, data.lng);
-        addMarkerWithTimeout(latLng, i * 200);
-    }*/
+        addMarkerWithTimeout(latLng, i * 300);
+    }
    /* ****************************************************/
      var bounds = {
         north: 180,
@@ -114,7 +114,7 @@ function AddMakers(){
        var ptLat = Math.random() * (bounds.north - bounds.south) + bounds.south;
        var ptLng = Math.random() * (bounds.east - bounds.west) + bounds.west;
        var pins = new google.maps.LatLng(ptLat,ptLng);
-       addMarkerWithTimeout(pins, i * 200);
+       addMarkerWithTimeout(pins, i * 100);
     } 
     /* ****************************************************/
    // console.log('marker no.: ' + markers.length);
@@ -134,8 +134,7 @@ function addMarkerWithTimeout(latLng, timeout) {
             labelAnchor: new google.maps.Point(10, 10),
             labelClass: "label", // the CSS class for the label
         }));
-        console.log('adding: ' + markers.length);
-         //console.log('marker length: ' + markers.length);
+        //console.log('adding: ' + markers.length);
         if (markers.length >100) {
             
             clearMarkers();
@@ -148,7 +147,6 @@ function addMarkerWithTimeout(latLng, timeout) {
 function clearMarkers() {
   for (var i = 0; i < markers.length; i++) {
     markers[i].setMap(null);
-    console.log('removing: ' + markers.length);
   }
   markers=[];
 }
