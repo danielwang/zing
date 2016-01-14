f0135635a70dcb7f044d69ec0d1ad4da6f65f58f
danielwang <danielw@pu-macair-1.queen.pageup.com.au>
2016-01-14 16:17:54 +1100

    add performance lifecycle

--- performance.php
+++ performance.php
@@ -1,11 +1,54 @@
 <?php include 'base.php' ?>
 <?php startblock('page') ?>
 
-
-
-<div class="row row-gap">  
  
-  <div class="col-xs-12">
+<div class="row row-gap"> 
+  <h4>Daniel Wang - My Contribution and Career Plan</h4> 
+  <ul class="lifecycle" id="lifecycle">
+    <li class="li complete">
+      <div class="timestamp">
+        <span class="date">31 Aug 2015<span>
+      </div>
+      <div class="status">
+        <h4>Planning</h4>
+      </div>
+    </li>
+    <li class="li complete">
+      <div class="timestamp">
+        <span class="date">22 Jan 2016<span>
+      </div>
+      <div class="status">
+        <h4> Track & Review </h4>
+      </div>
+    </li>
+    <li class="li">
+      <div class="timestamp">
+        <span class="date">31 Jul 2016<span>
+      </div>
+      <div class="status">
+        <h4> Track & Review (End of Year) </h4>
+      </div>
+    </li>
+    <li class="li">
+      <div class="timestamp">
+        <span class="date">31 Jul 2016<span>
+      </div>
+      <div class="status">
+        <h4> Complete</h4>
+      </div>
+    </li>
+   </ul>
+</div> 
+<div class="row"> 
+  <div class="col-xs-2">
+      <ul>
+        <li>Start</li>
+        <li>My Contribution</li>
+        <li>Career plan</li>
+        <li>Next Step</li>
+      </ul>  
+  </div> 
+  <div class="col-xs-10">
        <?php include '_partials/_item.html';?>
   </div>
 
