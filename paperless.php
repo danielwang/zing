bfca7ef3ed10006624bf1ee59acfd0b95951df2a
danielwang <danielw@pu-macair-1.queen.pageup.com.au>
2016-03-07 11:32:28 +1100

    paperless tuneup

---
+++ paperless.php
@@ -0,0 +1,196 @@
+<?php include 'base.php' ?>
+<?php startblock('page') ?>
+<link href="css/paperless.css" rel="stylesheet" type="text/css" />
+<div class="ipad row-gap">
+   <div class="ibox-content inspinia-timeline">
+      <div class="timeline-item passed">
+         <div class="row">
+            <div class="col-xs-1">
+               <div class="target-date">
+                   <div class="month">March</div>
+                   <div class="day">3</div>
+                   <div class="year">2016</div>
+               </div>   
+            </div>   
+            <div class="col-xs-2 date">
+               <i class="fa fa-lg fa-smile-o green"></i>
+               6:00 am
+            </div>
+            <div class="col-xs-7 content no-top-border">
+               <p class="m-b-xs"><strong>Daniel Wang</strong> <small> (First Interview) </small></p>
+               <div class="row">
+                  <span class="col-xs-6"><i class="fa fa-fw fa-calendar"></i> Friday 04 March 2016  6:00 am -> 7:00 am</span> 
+                  <span class="col-xs-6"><i class="fa fa-fw fa-map-marker"></i> London Room level 6 </span> 
+                  <span class="col-xs-6"><i class="fa fa-fw fa-suitcase"></i> Graphic Designer #58973654</span>
+                  <span class="col-xs-6"><i class="fa fa-fw fa-user"></i> Recruiter: Danielle Berto</span> 
+               </div>            
+            </div>
+         </div>
+      </div>
+      <div class="timeline-item passed">
+         <div class="row">
+            <div class="col-xs-3 date">
+               <i class="fa fa-lg fa-frown-o"></i>
+               7:00 am
+            </div>
+            <div class="col-xs-7 content">
+               <p class="m-b-xs"><strong>Sandy Mann</strong></p>
+               <div class="row">
+                  <span class="col-xs-6"><i class="fa fa-fw fa-calendar"></i> Friday 04 March 2016  6:00 am -> 7:00 am</span> 
+                  <span class="col-xs-6"><i class="fa fa-fw fa-map-marker"></i> London Room level 6 </span> 
+                  <span class="col-xs-6"><i class="fa fa-fw fa-suitcase"></i> Graphic Designer #58973654</span>
+                  <span class="col-xs-6"><i class="fa fa-fw fa-user"></i> Recruiter: Danielle Berto</span> 
+               </div>  
+            </div>
+         </div>
+      </div>
+      <div class="timeline-item passed">
+         <div class="row">
+            <div class="col-xs-3 date">
+               <i class="fa fa-lg fa-frown-o"></i>
+               8:00 am
+               <br>
+            </div>
+            <div class="col-xs-7 content">
+               <p class="m-b-xs"><strong>Elberto Pranoto</strong></p>
+               <div class="row">
+                  <span class="col-xs-6"><i class="fa fa-fw fa-calendar"></i> Friday 04 March 2016  6:00 am -> 7:00 am</span> 
+                  <span class="col-xs-6"><i class="fa fa-fw fa-map-marker"></i> London Room level 6 </span> 
+                  <span class="col-xs-6"><i class="fa fa-fw fa-suitcase"></i> Graphic Designer #58973654</span>
+                  <span class="col-xs-6"><i class="fa fa-fw fa-user"></i> Recruiter: Danielle Berto</span> 
+               </div> 
+            </div>
+         </div>
+      </div>
+      <div class="timeline-item passed">
+         <div class="row">
+            <div class="col-xs-3 date">
+               <i class="fa fa-lg fa-smile-o green"></i>
+               12:50 pm
+            </div>
+            <div class="col-xs-7 content">
+               <p class="m-b-xs"><strong>Peter Polacek</strong></p>
+               <div class="row">
+                  <span class="col-xs-6"><i class="fa fa-fw fa-calendar"></i> Friday 04 March 2016  6:00 am -> 7:00 am</span> 
+                  <span class="col-xs-6"><i class="fa fa-fw fa-map-marker"></i> London Room level 6 </span> 
+                  <span class="col-xs-6"><i class="fa fa-fw fa-suitcase"></i> Graphic Designer #58973654</span>
+                  <span class="col-xs-6"><i class="fa fa-fw fa-user"></i> Recruiter: Danielle Berto</span> 
+               </div> 
+            </div>
+         </div>
+      </div>
+      <div class="timeline-item">
+         <div class="row">
+            <div class="col-xs-3 date">
+               2:00 pm
+               <br>
+               <small class="text-navy red">in 30 mins</small>
+            </div>
+            <div class="col-xs-7 content">
+               <p class="m-b-xs"><strong>Marcus Wong</strong></p>
+              <div class="row">
+                  <span class="col-xs-6"><i class="fa fa-fw fa-calendar"></i> Friday 04 March 2016  6:00 am -> 7:00 am</span> 
+                  <span class="col-xs-6"><i class="fa fa-fw fa-map-marker"></i> London Room level 6 </span> 
+                  <span class="col-xs-6"><i class="fa fa-fw fa-suitcase"></i> Graphic Designer #58973654</span>
+                  <span class="col-xs-6"><i class="fa fa-fw fa-user"></i> Recruiter: Danielle Berto</span> 
+               </div> 
+            </div>
+         </div>
+      </div>
+      <div class="timeline-item ">
+         <div class="row">
+            <div class="col-xs-3 date">    
+               4:00 pm
+               <br>
+               <small class="text-navy red">in 2 hours</small>
+            </div>
+            <div class="col-xs-7 content">
+               <p class="m-b-xs"><strong>Steve Oxley</strong></p>
+               <div class="row">
+                  <span class="col-xs-6"><i class="fa fa-fw fa-calendar"></i> Friday 04 March 2016  6:00 am -> 7:00 am</span> 
+                  <span class="col-xs-6"><i class="fa fa-fw fa-map-marker"></i> London Room level 6 </span> 
+                  <span class="col-xs-6"><i class="fa fa-fw fa-suitcase"></i> Graphic Designer #58973654</span>
+                  <span class="col-xs-6"><i class="fa fa-fw fa-user"></i> Recruiter: Danielle Berto</span> 
+               </div> 
+            </div>
+         </div>
+      </div>
+      <div class="timeline-item">
+         <div class="row">
+            <div class="col-xs-1">
+               <div class="target-date">
+                   <div class="month">March</div>
+                   <div class="day">4</div>
+                   <div class="year">2016</div>
+               </div>   
+            </div>   
+            <div class="col-xs-2 date">
+               6:00 am
+            </div>
+            <div class="col-xs-7 content no-top-border">
+               <p class="m-b-xs"><strong>Daniel Wang</strong> <small> (First Interview) </small></p>
+               <div class="row">
+                  <span class="col-xs-6"><i class="fa fa-fw fa-calendar"></i> Friday 04 March 2016  6:00 am -> 7:00 am</span> 
+                  <span class="col-xs-6"><i class="fa fa-fw fa-map-marker"></i> London Room level 6 </span> 
+                  <span class="col-xs-6"><i class="fa fa-fw fa-suitcase"></i> Graphic Designer #58973654</span>
+                  <span class="col-xs-6"><i class="fa fa-fw fa-user"></i> Recruiter: Danielle Berto</span> 
+               </div>            
+            </div>
+         </div>
+      </div>
+      <div class="timeline-item">
+         <div class="row">
+            <div class="col-xs-3 date">       
+               7:00 am
+            </div>
+            <div class="col-xs-7 content">
+               <p class="m-b-xs"><strong>Sandy Mann</strong></p>
+               <div class="row">
+                  <span class="col-xs-6"><i class="fa fa-fw fa-calendar"></i> Friday 04 March 2016  6:00 am -> 7:00 am</span> 
+                  <span class="col-xs-6"><i class="fa fa-fw fa-map-marker"></i> London Room level 6 </span> 
+                  <span class="col-xs-6"><i class="fa fa-fw fa-suitcase"></i> Graphic Designer #58973654</span>
+                  <span class="col-xs-6"><i class="fa fa-fw fa-user"></i> Recruiter: Danielle Berto</span> 
+               </div>  
+            </div>
+         </div>
+      </div>
+      <div class="timeline-item">
+         <div class="row">
+            <div class="col-xs-3 date">
+              
+               8:00 am
+               <br>
+            </div>
+            <div class="col-xs-7 content">
+               <p class="m-b-xs"><strong>Elberto Pranoto</strong></p>
+               <div class="row">
+                  <span class="col-xs-6"><i class="fa fa-fw fa-calendar"></i> Friday 04 March 2016  6:00 am -> 7:00 am</span> 
+                  <span class="col-xs-6"><i class="fa fa-fw fa-map-marker"></i> London Room level 6 </span> 
+                  <span class="col-xs-6"><i class="fa fa-fw fa-suitcase"></i> Graphic Designer #58973654</span>
+                  <span class="col-xs-6"><i class="fa fa-fw fa-user"></i> Recruiter: Danielle Berto</span> 
+               </div> 
+            </div>
+         </div>
+      </div>
+      <div class="timeline-item">
+         <div class="row">
+            <div class="col-xs-3 date">
+               
+               12:50 pm
+            </div>
+            <div class="col-xs-7 content">
+               <p class="m-b-xs"><strong>Peter Polacek</strong></p>
+               <div class="row">
+                  <span class="col-xs-6"><i class="fa fa-fw fa-calendar"></i> Friday 04 March 2016  6:00 am -> 7:00 am</span> 
+                  <span class="col-xs-6"><i class="fa fa-fw fa-map-marker"></i> London Room level 6 </span> 
+                  <span class="col-xs-6"><i class="fa fa-fw fa-suitcase"></i> Graphic Designer #58973654</span>
+                  <span class="col-xs-6"><i class="fa fa-fw fa-user"></i> Recruiter: Danielle Berto</span> 
+               </div> 
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+
+
+<?php endblock() ?>
