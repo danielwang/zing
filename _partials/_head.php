2d0ec03e49782129ecd41b06fc8dc60051e51234
Daniel Wang <daniel.wang@pageuppeople.com>
2016-11-22 15:01:36 +1100

    test 2

--- _partials/_head.php
+++ _partials/_head.php
@@ -8,21 +8,20 @@
     <meta name="author" content="daniel.wang" >
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-    <link href="<?php echo($resURL)?>../gel/pu-sg/dist/css/pageup.css" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet/less" type="text/css" href="<?php echo($resURL) ?>css/main.less" />
+    <link href="http://styleguide.pageuppeople.com/dist/css/pageup.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet/less" type="text/css" href="<?php echo ($resURL) ?>css/main.less" />
     <!-- apps css -->
-    <?php if($pageClass != null) { 
-       echo '<link rel="stylesheet/less" type="text/css" href="'.$resURL.'css/apps.less" />';
-    }?>
+    <?php if ($pageClass != null) {
+	echo '<link rel="stylesheet/less" type="text/css" href="' . $resURL . 'css/apps.less" />';
+}?>
     <!-- LESS.js -->
     <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.5.1/less.min.js"></script>
     <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-latest.js"></script>
     <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>  
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
     <!-- custom script -->
-    <script src="<?php echo($resURL) ?>js/custom.js"></script> 
+    <script src="<?php echo ($resURL) ?>js/custom.js"></script>
 </head>
-<body class="<?php echo($pageClass)?>">
+<body class="<?php echo ($pageClass) ?>">
 
-    
