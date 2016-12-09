262a0751ca7ed762d91d451fb11f7fe28dd707ab
Daniel Wang <daniel.wang@pageuppeople.com>
2016-12-09 12:02:34 +1100

    modified the complie script

--- g.php
+++ g.php
@@ -1,23 +1,22 @@
 
  <?php
-// ob_start();
-// include_once 'layouts/fluid.php';
+ compileIndex();
+ function compileIndex(){
+   $url = 'http://localhost:9000/mockup/index.php';
+   $ch = curl_init();
+   // tell cURL what the URL is
+   curl_setopt($ch, CURLOPT_URL, $url);
+   // tell cURL that you want the data back from that URL
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+   // run cURL
+   $output = curl_exec($ch);
+   // end the cURL call (this also cleans up memory so it is
+   // important)
+   curl_close($ch);
+   // display the output
+   file_put_contents("index.html", $output);
+ }
 
-// file_put_contents("fluid.html", ob_get_contents());
-// ob_end_clean(); // clear the buffer
-// echo "<p>latest static index has been generated</p>";
-
-// $pages = array("form.php", "list.php", "dock.php", "systemsettings.php", "journal.php", "typography.php", "index.php", "home.php", "dashboard.php", "learning.php", "performance.php");
-// $arrlength = count($pages);
-
-// for ($x = 0; $x < $arrlength; $x++) {
-// 	if (file_exists($pages[$x])) {
-// 		generateStaticPage($pages[$x]);
-// 		echo "<p><strong>$pages[$x]</strong> has been generated</p>";
-// 	} else {
-// 		echo "<p style='color:red'><strong>$pages[$x]</strong> doesn't exist</p>";
-// 	}
-// }
 
 /* read php files from folders */
 $folders = array("layouts", "modules", "patterns");
@@ -26,7 +25,7 @@ for ($i = 0; $i < count($folders); $i++) {
 }
 
 function readFolders($foldername) {
-	echo "reading $foldername folder <br>";
+	echo "<h3>reading $foldername folder </h3>";
 	$files = array();
 	$handle = opendir($foldername . '/');
 	while (false !== ($file = readdir($handle))):
@@ -38,21 +37,41 @@ function readFolders($foldername) {
 	chdir($foldername);
 	foreach ($files as $file):
 		echo "converting $file <br>";
-		generateStaticPage($foldername, $file);
+    viewSource($foldername, $file);
+		// generateStaticPage($foldername, $file);
 	endforeach;
 	chdir("../");
 }
-
 /* read php files then output html */
-function generateStaticPage($foldername, $page) {
-	ob_start();
-	$file = (string) $foldername . '/' . $page;
-	include_once "{$file}";
-	flushblocks();
-	$outputfile = str_replace(".php", "", "{$page}.html");
-	/* output to folders*/
-	//echo $outputfile;
-	file_put_contents($outputfile, ob_get_clean());
+function viewSource($foldername, $page){
+  // define the URL to load
+  $url = 'http://localhost:9000/mockup/'. $foldername . '/' . $page;
+  // start cURL
+  $ch = curl_init();
+  // tell cURL what the URL is
+  curl_setopt($ch, CURLOPT_URL, $url);
+  // tell cURL that you want the data back from that URL
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  // run cURL
+  $output = curl_exec($ch);
+  // end the cURL call (this also cleans up memory so it is
+  // important)
+  curl_close($ch);
+  // display the output
+  // echo $output;
+  $outputfile = str_replace(".php", "", "{$page}.html");
+  file_put_contents($outputfile, $output);
 }
-echo "<a href='d.php'>Delete all html files</a>";
-?>
+
+// function generateStaticPage($foldername, $page) {
+// 	ob_start();
+// 	$file = (string) $foldername . '/' . $page;
+// 	include_once "{$file}";
+// 	flushblocks();
+// 	$outputfile = str_replace(".php", "", "{$page}.html");
+// 	/* output to folders*/
+// 	//echo $outputfile;
+// 	file_put_contents($outputfile, ob_get_clean());
+// }
+// echo "<a href='d.php'>Delete all html files</a>";
+// ?>
