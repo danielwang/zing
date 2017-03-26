893e8f2a84f4ab6bc2e73777de19010f609b7906
Daniel Wang <daniel.wang@pageuppeople.com>
2017-03-27 08:52:42 +1100

    created person card page

--- head.php
+++ head.php
@@ -10,7 +10,7 @@
     <meta name="author" content="daniel.wang" >
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-    <link href="https://s3-ap-southeast-2.amazonaws.com/pu-gel/1.2/dist/css/pageup.css" rel="stylesheet" type="text/css" />
+    <!--<link href="https://ui.pageuppeople.com/gel/pu-theme/v1.1/css/pageup.min.css" rel="stylesheet" type="text/css" /> -->
     <link rel="stylesheet/less" type="text/css" href="<?php echo ($resURL) ?>css/main.less" />
     <!-- LESS.js -->
     <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.5.1/less.min.js"></script>
---
+++ patterns/cards.php
@@ -0,0 +1,5 @@
+<?php include '../base.php';?>
+<?php startblock('page');?>
+<h1>Person Cards</h1>
+<?php include '../_partials/_cards.html';?>
+<?php endblock()?>
