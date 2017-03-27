958ff0126fa329275458b8d66437cec8176b22a4
Daniel Wang <daniel.wang@pageuppeople.com>
2017-03-27 17:12:47 +1100

    job cards

--- head.php
+++ head.php
@@ -14,11 +14,6 @@
     <link rel="stylesheet/less" type="text/css" href="<?php echo ($resURL) ?>css/main.less" />
     <!-- LESS.js -->
     <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.5.1/less.min.js"></script>
-    <!-- jQuery -->
-    <script src="https://code.jquery.com/jquery-latest.js"></script>
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-    <!-- custom script -->
-    <script src="<?php echo ($resURL) ?>js/custom.js"></script>
+
 </head>
 <body>
