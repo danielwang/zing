c295801825264019edad2dfff81a041b3311eeae
Daniel Wang <daniel.wang@pageuppeople.com>
2017-04-18 09:42:57 +1000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	head.php

--- head.php
+++ head.php
@@ -7,7 +7,6 @@
     <title>PageUp Concepts Design</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
-    <meta name="robots" content="noindex">
     <meta name="author" content="daniel.wang" >
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
