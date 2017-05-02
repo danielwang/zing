69f93bbae0a7b3402e30fff66910f024b3875276
Daniel Wang <daniel.wang@pageuppeople.com>
2017-05-02 12:17:50 +1000

    update latest gel

--- modules/recruitment/job-card.php
+++ modules/recruitment/job-card.php
@@ -1,23 +1,24 @@
 <?php include '../../base.php' ?>
 <?php startblock('page') ?>
-
+<div class="page-header">
+  <h2>Human Resources Officer</h2>
+</div>
  <div id="dockbody">
-        <h2>Human Resources Officer</h2>
-        <div class="row row-gap">
-          <div class="col-sm-8">
-            <div class="panel panel-default">
-                   <div class="panel-heading">
-                       <h3 class="panel-title">Position info</h3>
-                   </div>
-                   <div class="panel-body">
-                  <?php include ("${resURL}components/_form.html"); ?>
-                </div>
-              </div>
+    <div class="row row-gap">
+      <div class="col-sm-8">
+        <div class="panel panel-default">
+               <div class="panel-heading">
+                   <h3 class="panel-title">Position info</h3>
+               </div>
+               <div class="panel-body">
+              <?php include ("${resURL}components/_form.html"); ?>
+            </div>
           </div>
-          <div class="col-sm-4">
-              <?php include ("${resURL}components/_tabs.html"); ?>
-          </div>
-        </div>
+      </div>
+      <div class="col-sm-4">
+          <?php include ("${resURL}components/_tabs.html"); ?>
       </div>
     </div>
+  </div>
+</div>
 <?php endblock() ?>
