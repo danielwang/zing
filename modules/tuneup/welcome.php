c691db0df40dec4e959f3ca5ddebc5e8e7eb9f6f
danielwang <danielw@pulocals-Air-2.queen.pageup.com.au>
2017-07-07 15:06:31 +1000

    tuneup welcome page

---
+++ modules/tuneup/welcome.php
@@ -0,0 +1,35 @@
+<?php include '../../base.php';?>
+<?php startblock('page');?>
+<div class="modal fade in" id="ideaModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" style="display: block; padding-right: 15px;">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span class="fa fa-times-circle" aria-hidden="true"></span>
+                </button>
+        <h4 class="modal-title" id="myModalLabel">Welcome</h4>
+      </div>
+      <div class="modal-body text-center">
+        <h3>Welcome to PageUp Idea Hub</h3>
+            <hr>
+            <svg width="150" xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 37.12 37.13">
+              <defs><style>.cls-1{fill:none;stroke:#57C0E9;stroke-linecap:round;stroke-linejoin:round;stroke-width:0.3px;}</style></defs>
+              <title>Artboard 2</title>
+              <line class="cls-1" x1="13.92" y1="8.87" x2="23.21" y2="8.87"/>
+              <circle class="cls-1" cx="16.37" cy="4.71" r="1.28"/>
+              <circle class="cls-1" cx="20.75" cy="2.6" r="1.28"/>
+              <path class="cls-1" d="M26.26,25.16l-4.2-5.81V8.87h-7V19.35l-4.2,5.81L5.64,32.38A2.15,2.15,0,0,0,7.39,35.8H29.74a2.15,2.15,0,0,0,1.75-3.42Z"/>
+              <path class="cls-1" d="M10.87,25.16c7.7,3,6.24-3.75,14.07-1.83"/>
+              <circle class="cls-1" cx="22.61" cy="26.85" r="1.28"/>
+              <circle class="cls-1" cx="19.18" cy="31.39" r="1.82"/>
+              <head/>
+             </svg>
+             <hr>
+             <p>We’d love your feedback on great new functionality <br>that we are thinking about!</p>
+             <p> <button class="btn btn-info">Let's go!</button></p>
+        </div>
+        <div class="modal-footer"></div>
+    </div>
+  </div>
+</div>
+<?php endblock()?>
