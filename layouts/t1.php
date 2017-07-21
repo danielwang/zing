29df2bf012d4ecb3486deeaa169ddc4aad8abea8
Daniel  Wang <danielwang130@gmail.com>
2017-07-21 12:37:12 +1000

    t1

--- layouts/t1.php
+++ layouts/t1.php
@@ -37,7 +37,7 @@
 <?php startblock('page');?>
 <div class="row row-gap">
     <div class="col-sm-8">
-        <img class="img-responsive" src="http://placehold.it/780x800/ABE0F4/57C0E9?text=main" alt="">
+        <img class="img-responsive" src="http://placehold.it/780x1800/ABE0F4/57C0E9?text=main" alt="">
     </div>
 
     <div class="col-sm-4">
